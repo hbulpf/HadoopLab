44aa1bdf211d4ba5b701957b4aaf7763f53b93df
lipengfei <hudalpf@163.com>
2018-10-22 22:08:54 +0800

    hnbd hadoop

--- auto/install_hadoop.sh
+++ auto/install_hadoop.sh
@@ -6,7 +6,7 @@ yum update -y && yum install -y openssh-server java-1.8.0-openjdk-devel wget
 # mkdir /tmp
 cd /tmp && \
     wget -O hadoop-2.7.7.tar.gz http://mirror.bit.edu.cn/apache/hadoop/common/hadoop-2.7.7/hadoop-2.7.7.tar.gz && \
-    tar -xvzf hadoop-2.7.7.tar.gz -C /usr/local/hadoop && \
+    mkdir -p /usr/local/hadoop  && tar -xvzf hadoop-2.7.7.tar.gz -C /usr/local/hadoop && \
     rm hadoop-2.7.7.tar.gz -y
 # chmod a+x install_nfs_client.sh && ./install_nfs_client.sh
 # mkdir -p /usr/local/hadoop && \
