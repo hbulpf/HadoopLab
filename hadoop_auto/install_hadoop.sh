9d595a1ccdd04645bbc61dff5509cf545bc73367
LPF-OFFICE <hudalpf@163.com>
2018-12-17 17:05:11 +0800

    修复auto hadoop 问题

--- hadoop_auto/install_hadoop.sh
+++ hadoop_auto/install_hadoop.sh
@@ -2,17 +2,21 @@
 # install openssh-server, openjdk and wget
 yum update -y && yum install -y openssh-server java-1.8.0-openjdk-devel wget
 
+# download hadoop 2.7.2
+if [ ! -d /download ]; then
+	mkdir -p /download
+fi
+if [ ! -f /download/hadoop-2.7.7.tar.gz ]; then
+	wget -O /download/hadoop-2.7.7.tar.gz http://mirror.bit.edu.cn/apache/hadoop/common/hadoop-2.7.7/hadoop-2.7.7.tar.gz
+fi
+
 # install hadoop 2.7.2
-# mkdir /tmp
-cd /tmp && \
-    wget -O hadoop-2.7.7.tar.gz http://mirror.bit.edu.cn/apache/hadoop/common/hadoop-2.7.7/hadoop-2.7.7.tar.gz && \
-    mkdir -p /usr/local/hadoop  && tar -xvzf hadoop-2.7.7.tar.gz -C /usr/local/hadoop && \
-    rm hadoop-2.7.7.tar.gz -f
+cd /download && \
+    mkdir -p /usr/local/hadoop  && tar -xvzf hadoop-2.7.7.tar.gz -C /usr/local/hadoop
 # chmod a+x install_nfs_client.sh && ./install_nfs_client.sh
-# mkdir -p /usr/local/hadoop && \
-#     tar -xvzf /nfs_mirrors/hadoop/hadoop-2.7.7.tar.gz -C /usr/local/hadoop
 
 useradd -d /home/hadoop -m hadoop
 echo 'hadoop' | passwd --stdin hadoop
 usermod -aG hadoop hadoop
+sed -i "/^root/a\hadoop ALL = (ALL:ALL) NOPASSWD:ALL" /etc/sudoers
 chown -R hadoop:hadoop /usr/local/hadoop/hadoop-2.7.7
